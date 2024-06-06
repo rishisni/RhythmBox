7460107e386a64549950485fbe7e1a555a639fe3
Rishabh Saini <107872269+rishisni@users.noreply.github.com>
2024-06-06 13:57:00 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -8,8 +8,4 @@ class Config:
     CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
     CELERY_TIMEZONE = 'Asia/Kolkata'
     # Email configuration
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'rishabhkirodiwal04@gmail.com'
-    MAIL_PASSWORD = 'qjyfmsblpzdfnrjq '
+   
